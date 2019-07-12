e5291335c07e55391b89bbc44eed59de4d00dead
marcoaqil <m.aqil@spinozacentre.nl>
2019-07-12 15:39:06 +0200

    screenshot to dm function and initial notebook

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,35 @@
-create_dm_from_screenshots(screenshot_path)
+import os
+import numpy as np
+import imageio
+
+
+
+def create_dm_from_screenshots(screenshot_path,
+                               n_pix=40,
+                               ):
+
+    image_list = os.listdir(screenshot_path)
+
+    # there is one more MR image than screenshot
+    design_matrix = np.zeros((n_pix, n_pix, 1+len(image_list)))
+    for image_file in image_list:
+        # assuming last three numbers before .png are the screenshot number
+        img_number = int(image_file[-7:-4])
+        # subtract one to start from zero
+        img = imageio.imread(os.path.join(screenshot_path, image_file))
+        # make it square
+        if img.shape[0] != img.shape[1]:
+            offset = int((img.shape[1]-img.shape[0])/2)
+            img = img[:, offset:(offset+img.shape[0])]
+
+        # downsample
+        downsampling_constant = int(img.shape[1]/n_pix)
+        downsampled_img = img[::downsampling_constant, ::downsampling_constant]
+
+        # binarize image into dm matrix
+        # assumes: standard RGB255 format; only colors present in image are black, white, grey, red, green.
+        design_matrix[:, :, img_number][np.where(((downsampled_img[:, :, 0] == 0) & (
+            downsampled_img[:, :, 1] == 0)) | ((downsampled_img[:, :, 0] == 255) & (downsampled_img[:, :, 1] == 255)))] = 1
+    
+    
+    return design_matrix
