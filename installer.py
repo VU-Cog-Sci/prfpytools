0649d7cae6d93536b51cc39bef36a92e7ae89747
marcoaqil <m.aqil@spinozacentre.nl>
2022-12-12 11:27:39 +0100

    HCP viz adjust

--- installer.py
+++ installer.py
@@ -6,7 +6,7 @@ assert "Python 3." in os.popen("python -V").read(), "Get python 3"
 os.popen("conda config --add channels conda-forge").read()
 
 print("Installing dependencies with conda...")
-os.popen(f"conda install --yes numpy scipy cython h5py matplotlib ipython tqdm yaml pyyaml cifti nibabel nilearn joblib scikit-learn statsmodels cmasher").read()
+os.popen(f"conda install --yes numpy scipy cython h5py matplotlib ipython dill tqdm yaml pyyaml cifti nibabel nilearn joblib scikit-learn statsmodels cmasher").read()
 
 assert "prfpy" in os.popen("conda list").read(), "Please install prfpy manually. (git clone https://github.com/VU-Cog-Sci/prfpy.git; cd prfpy; python installer.py)"
 assert "pycortex" in os.popen("conda list").read(), "Please install pycortex manually. (git clone https://github.com/gallantlab/pycortex.git; cd pycortex; python -m pip install -e .)"
