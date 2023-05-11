bd940e64b2da308c48e5f02eefa31999f3e0597d
marcoaqil <m.aqil@spinozacentre.nl>
2023-05-11 13:09:34 +0200

    grid

--- prf_fit.py
+++ prf_fit.py
@@ -535,17 +535,17 @@ if norm_model_variant == "abcd":
     # neural_baseline_grid=np.array([0,1,10,100], dtype='float32')
     # surround_baseline_grid=np.array([0.1,1.0,10.0,100.0], dtype='float32')
 
-    surround_amplitude_grid=np.array([0.05,0.2,0.4,0.7,1,3], dtype='float32')
-    surround_size_grid=np.array([0.5,1,2.5,4,7], dtype='float32')
-    neural_baseline_grid=np.array([0,1,10,100], dtype='float32')
-    surround_baseline_grid=np.array([0.1,1.0,10.0,100.0], dtype='float32')
+    # surround_amplitude_grid=np.array([0.05,0.2,0.4,0.7,1,3], dtype='float32')
+    # surround_size_grid=np.array([0.5,1,2.5,4,7], dtype='float32')
+    # neural_baseline_grid=np.array([0,1,10,100], dtype='float32')
+    # surround_baseline_grid=np.array([0.1,1.0,10.0,100.0], dtype='float32')
 
 
     #finer grid
-    # surround_amplitude_grid=np.array([0.001,0.01,0.05,0.1,0.2,0.5,0.75,1,2], dtype='float32')
-    # surround_size_grid=np.array([0.5,0.75,1,1.05,1.1,1.5,2.1,3,4.5,6,8], dtype='float32')
-    # neural_baseline_grid=np.array([0,1,2,5,10,20,50,75,100], dtype='float32')
-    # surround_baseline_grid=np.array([0.1,1.0,2,5,10.0,20,50,75,100.0], dtype='float32')
+    surround_amplitude_grid=np.array([0.001,0.01,0.05,0.1,0.2,0.5,0.75,1,2], dtype='float32')
+    surround_size_grid=np.array([0.5,0.75,1,1.1,1.5,3,5,8], dtype='float32')
+    neural_baseline_grid=np.array([0,1,2,5,10,20,50,75,100], dtype='float32')
+    surround_baseline_grid=np.array([0.1,1.0,2,5,10.0,20,50,75,100.0], dtype='float32')
 
 
 elif norm_model_variant == "abc":
